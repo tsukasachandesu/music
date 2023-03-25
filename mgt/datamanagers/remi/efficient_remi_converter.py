26e3bf04ffb05073de90d42aa1d79f80a844066b
tsukasachandesu <44788261+tsukasachandesu@users.noreply.github.com>
2023-03-25 21:18:31 +0900

    Update efficient_remi_converter.py

--- mgt/datamanagers/remi/efficient_remi_converter.py
+++ mgt/datamanagers/remi/efficient_remi_converter.py
@@ -40,9 +40,9 @@ class EfficientRemiConverter(object):
          
             if event.name == 'Bar':
                 items.append(RemiItem(type=RemiEventType.BAR, original_events=[event]))
-            elif event.name == 'Position' and len(events) > index + 2 and events[index + 1].name == 'Chord':
-                print(event)
+            elif event.name == 'Chord':
                 original_events = [event, events[index + 1], events[index + 2]]
+                print(RemiItem(type=RemiEventType.TEMPO, position=position, original_events=original_events))
                 position = int(event.value.partition("/")[0])
                 items.append(RemiItem(type=RemiEventType.TEMPO, position=position, original_events=original_events))
             elif event.name == 'Position' and len(events) > index + 5 and events[index + 1].name == 'Instrument' and events[index + 3].name == 'Note Name':
