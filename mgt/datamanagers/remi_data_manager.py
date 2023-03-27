f7459f767f82d454b6aa5a3d1d35cf0558b89366
tsukasachandesu <44788261+tsukasachandesu@users.noreply.github.com>
2023-03-27 23:01:14 +0900

    Update remi_data_manager.py

--- mgt/datamanagers/remi_data_manager.py
+++ mgt/datamanagers/remi_data_manager.py
@@ -191,6 +191,7 @@ class RemiDataManager(DataManager):
                         total1 = [0,0.2,0.4,0.6,0.8,1,1.2,1.4]
                         diamet1 = [0,0.3,0.6,0.9,1.2,1.5,1.8,2.1,2.4,2.7,3.0,3.3,3.6,3.9,4.2]
                         centroid1 = [0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1,1.1,1.2,1.3,1.4,1.5,1.6]
+                        print(total_tension.size)
 
                         numi = 0
                         for (i,j) in enumerate(words):
