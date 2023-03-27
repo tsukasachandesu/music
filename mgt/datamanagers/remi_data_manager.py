afe1ffbb29465ee940ecafb4a81b57c8e31c1747
tsukasachandesu <44788261+tsukasachandesu@users.noreply.github.com>
2023-03-27 23:07:54 +0900

    Update remi_data_manager.py

--- mgt/datamanagers/remi_data_manager.py
+++ mgt/datamanagers/remi_data_manager.py
@@ -185,7 +185,9 @@ class RemiDataManager(DataManager):
                         print(total_tension)
                         print(diameters)
                         print(centroid_diff)
-                                               
+   
+
+
 
                         
                         total1 = [0,0.2,0.4,0.6,0.8,1,1.2,1.4]
@@ -207,6 +209,113 @@ class RemiDataManager(DataManager):
                             numin = numin + 1
                         data = self.data_extractor.words_to_data(words)
                         print(data)
+                        
+                        
+                       
+                    
+                        sixteenth_time, beat_time, down_beat_time, beat_indices, down_beat_indices = get_beat_time(pm, beat_division=16)
+                        piano_roll = get_piano_roll(pm, sixteenth_time)
+
+                        key_name = all_key_names
+                        key_name, key_pos, note_shift = cal_key(piano_roll, key_name, end_ratio=0.5)
+                        centroids = cal_centroid(piano_roll, note_shift, -1, -1)
+                        window_size = 1
+                        if window_size == 1:
+                            # use a bar window to detect key change
+                            merged_centroids = merge_tension(
+                                centroids, beat_indices, down_beat_indices, window_size=-1)
+
+                            silent = np.where(np.linalg.norm(merged_centroids, axis=-1) == 0)
+                            merged_centroids = np.array(merged_centroids)
+
+                            key_diff = merged_centroids - key_pos
+                            key_diff = np.linalg.norm(key_diff, axis=-1)
+
+                            key_diff[silent] = 0
+
+                            diameters = cal_diameter(piano_roll, note_shift, -1, -1)
+                            diameters = merge_tension(
+                                diameters, beat_indices, down_beat_indices, window_size=-1)
+                            #
+
+                            key_change_bar = detect_key_change(
+                                key_diff, diameters, start_ratio=0.5)
+                            if key_change_bar != -1:
+                                key_change_beat = np.argwhere(
+                                    beat_time == down_beat_time[key_change_bar])[0][0]
+                                change_time = down_beat_time[key_change_bar]
+                                changed_key_name, changed_key_pos, changed_note_shift = get_key_index_change(
+                                    pm, change_time, sixteenth_time)
+                                if changed_key_name != key_name:
+                                    m = int(change_time // 60)
+                                    s = int(change_time % 60)
+
+                                else:
+                                    changed_note_shift = -1
+                                    changed_key_name = ''
+                                    key_change_beat = -1
+                                    change_time = -1
+                                    key_change_bar = -1
+
+                            else:
+                                changed_note_shift = -1
+                                changed_key_name = ''
+                                key_change_beat = -1
+                                change_time = -1
+
+                        else:
+                            changed_note_shift = -1
+                            changed_key_name = ''
+                            key_change_beat = -1
+                            change_time = -1
+                            key_change_bar = -1
+
+                        centroids = cal_centroid(
+                            piano_roll, note_shift, key_change_beat, changed_note_shift)
+
+                        merged_centroids = merge_tension(
+                            centroids, beat_indices, down_beat_indices, window_size=window_size)
+                        merged_centroids = np.array(merged_centroids)
+
+                        silent = np.where(np.linalg.norm(merged_centroids, axis=-1) < 0.1)
+
+                        if window_size == -1:
+                            window_time = down_beat_time
+                        else:
+                            window_time = beat_time[::window_size]
+
+                        if key_change_beat != -1:
+                            key_diff = np.zeros(merged_centroids.shape[0])
+                            changed_step = int(key_change_beat / abs(window_size))
+                            for step in range(merged_centroids.shape[0]):
+                                if step < changed_step:
+                                    key_diff[step] = np.linalg.norm(
+                                        merged_centroids[step] - key_pos)
+                                else:
+                                    key_diff[step] = np.linalg.norm(
+                                        merged_centroids[step] - changed_key_pos)
+                        else:
+                            key_diff = np.linalg.norm(merged_centroids - key_pos, axis=-1)
+                        key_diff[silent] = 0
+
+                        diameters = cal_diameter(
+                            piano_roll, note_shift, key_change_beat, changed_note_shift)
+                        diameters = merge_tension(
+                            diameters, beat_indices, down_beat_indices, window_size)
+                        #
+                        diameters[silent] = 0
+
+                        centroid_diff = np.diff(merged_centroids, axis=0)
+                        #
+                        np.nan_to_num(centroid_diff, copy=False)
+
+                        centroid_diff = np.linalg.norm(centroid_diff, axis=-1)
+                        centroid_diff = np.insert(centroid_diff, 0, 0)
+
+                        total_tension = key_diff
+                        print(total_tension)
+                        print(diameters)
+                        print(centroid_diff)  
                         training_data.append(data)
                     else:
                         data = self.data_extractor.extract_data(path, transposition_step)
