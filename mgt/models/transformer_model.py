10ce11d411a03472709a82c1eb86d46ce71324b8
tsukasachandesu <44788261+tsukasachandesu@users.noreply.github.com>
2023-04-03 22:58:13 +0900

    Update transformer_model.py

--- mgt/models/transformer_model.py
+++ mgt/models/transformer_model.py
@@ -112,8 +112,8 @@ class TransformerModel(object):
           depth = 6,                     # depth
           dim_head = 64,                  # attention head dimensions
           heads = 8,                     # number of attention heads
-          max_seq_len = 3000,             # the total receptive field of the transformer, in the paper this was 2 * block size
-          block_width = 1500,              # block size - total receptive field is max_seq_len, 2 * block size in paper. the block furthest forwards becomes the new cached xl memories, which is a block size of 1 (please open an issue if i am wrong)
+          max_seq_len = 2048,             # the total receptive field of the transformer, in the paper this was 2 * block size
+          block_width = 1024,              # block size - total receptive field is max_seq_len, 2 * block size in paper. the block furthest forwards becomes the new cached xl memories, which is a block size of 1 (please open an issue if i am wrong)
           xl_memories_layers = (5, 6),    # which layers to use xl memories. very old deepmind papers have shown you only need the last penultimate layers to have cached key values to see majority of benefit
           num_state_vectors = 512,        # number of state vectors, i believe this was a single block size in the paper, but can be any amount
           recurrent_layers = (4,),        # where to place the recurrent layer(s) for states with fixed simple gating
