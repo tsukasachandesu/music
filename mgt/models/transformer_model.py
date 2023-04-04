2ebe734b6804e8a3fd08daaecd77bf8615cf5fb6
tsukasachandesu <44788261+tsukasachandesu@users.noreply.github.com>
2023-04-04 20:11:34 +0900

    Update transformer_model.py

--- mgt/models/transformer_model.py
+++ mgt/models/transformer_model.py
@@ -110,7 +110,7 @@ class TransformerModel(object):
 
     def create_model(self):
         model = Mega(num_tokens = self.dictionary.size(),dim = 512,depth = 12,ema_heads = 16,attn_dim_qk = 128, attn_dim_value = 1024)
-        model = AutoregressiveWrapper(model, ignore_index=0, pad_value=0).to(utils.get_device())
+        model = AutoregressiveWrapper(model, pad_value=0).to(utils.get_device())
         return model
 
     def create_optimizer(self):
