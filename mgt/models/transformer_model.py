049be8ca1005f50cd9fe56d907d99900feb3a0f8
tsukasachandesu <44788261+tsukasachandesu@users.noreply.github.com>
2023-04-08 18:54:14 +0900

    Update transformer_model.py

--- mgt/models/transformer_model.py
+++ mgt/models/transformer_model.py
@@ -5,8 +5,8 @@ import time
 import torch
 import numpy as np
 
-from mega_pytorch.mega_pytorch import Mega
-from mega_pytorch.autoregressive_wrapper import AutoregressiveWrapper
+from palm_pytorch.triton import PaLM
+from palm_pytorch.autoregressive_wrapper import AutoregressiveWrapper
 
 from mgt.datamanagers.data_manager import Dictionary
 from mgt.models import utils
@@ -106,13 +106,8 @@ class TransformerModel(object):
         return sample.cpu().detach().numpy()[0]
 
     def create_model(self):
-        
-        model = Mega(
-            num_tokens = self.dictionary.size(),
-            dim = 512,
-            depth = 8
-        )
-        model = AutoregressiveWrapper(model).to(utils.get_device())   
+        model = PaLM(num_tokens=self.dictionary.size(), dim=512, depth=8)
+        model = AutoregressiveWrapper(model, max_seq_len=1024).to(utils.get_device())   
 
         return model
 
