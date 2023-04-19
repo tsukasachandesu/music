d5eababddd6f2db8df22843e487af378d79e7c65
tsukasachandesu <44788261+tsukasachandesu@users.noreply.github.com>
2023-04-19 22:36:45 +0900

    Update dictionary_generator.py

--- mgt/datamanagers/remi/dictionary_generator.py
+++ mgt/datamanagers/remi/dictionary_generator.py
@@ -20,6 +20,7 @@ class DictionaryGenerator(object):
         # First word is reserved for padding
         append_to_dictionary("pad")
         append_to_dictionary("mask")
+        append_to_dictionary('Bar_None')
 
         chords = ['Chord_A#:aug', 'Chord_A#:dim', 'Chord_A#:dom', 'Chord_A#:maj', 'Chord_A#:min', 'Chord_A:aug',
                   'Chord_A:dim', 'Chord_A:dom', 'Chord_A:maj', 'Chord_A:min', 'Chord_B:aug', 'Chord_B:dim',
@@ -65,7 +66,7 @@ class DictionaryGenerator(object):
         for i in range(32):
             append_to_dictionary(f"Note Velocity_{i}")
             
-        append_to_dictionary('Bar_None')
+        
 
         return Dictionary(dictionary[0], dictionary[1])
     
