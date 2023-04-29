1a48a8f67ec0f8f407b51abd0b0b7ce291645589
tsukasachandesu <44788261+tsukasachandesu@users.noreply.github.com>
2023-04-29 12:43:07 +0900

    Update dictionary_generator.py

--- mgt/datamanagers/remi/dictionary_generator.py
+++ mgt/datamanagers/remi/dictionary_generator.py
@@ -40,7 +40,6 @@ class DictionaryGenerator(object):
                   'Chord_G:aug', 'Chord_G:dim', 'Chord_G:dom', 'Chord_G:maj', 'Chord_G:min', 'Chord_N:N']
 
         tempo_classes = ['Tempo Class_fast', 'Tempo Class_mid', 'Tempo Class_slow']
-       
 
         for i in range(16):
             append_to_dictionary(f"Position_{i + 1}/16")
@@ -48,7 +47,7 @@ class DictionaryGenerator(object):
         for i in range(7680):
             append_to_dictionary(f"Pitchdur_{i}")
 
-        for i in range(2):
+        for i in range(129):
             append_to_dictionary(f"Instrument_{i}")
 
         for i in range(64):
@@ -66,7 +65,5 @@ class DictionaryGenerator(object):
         for i in range(32):
             append_to_dictionary(f"Note Velocity_{i}")
             
-        
-
         return Dictionary(dictionary[0], dictionary[1])
     
