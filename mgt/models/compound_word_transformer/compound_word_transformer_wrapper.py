b141ecc58820e3dab7a6cf5453d7649929b79df1
tsukasachandesu <44788261+tsukasachandesu@users.noreply.github.com>
2023-05-09 06:02:49 +0900

    Update compound_word_transformer_wrapper.py

--- mgt/models/compound_word_transformer/compound_word_transformer_wrapper.py
+++ mgt/models/compound_word_transformer/compound_word_transformer_wrapper.py
@@ -90,7 +90,9 @@ class CompoundWordTransformerWrapper(nn.Module):
         # individual output
         self.proj_type = nn.Linear(dim, self.num_tokens[0])
         self.proj_barbeat = nn.Linear(dim, self.num_tokens[1])
-        self.proj_note_name = nn.Sequential(nn.Linear(dim, self.num_tokens[2]),　nn.Tanh(), nn.Linear(dim, self.num_tokens[2]))
+        self.proj_note_name = nn.Sequential(nn.Linear(dim, self.num_tokens[2]), nn.Tanh(), nn.Linear(self.num_tokens[2], self.num_tokens[2]))
+        
+
 
         # in_features is equal to dimension plus dimensions of the type embedding
         self.project_concat_type = nn.Linear(dim + self.emb_sizes[0], dim)
