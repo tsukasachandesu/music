e9414026e6d89a18f61b7d462ff7089b42945e05
tsukasachandesu <44788261+tsukasachandesu@users.noreply.github.com>
2023-05-11 23:10:03 +0900

    Update compound_word_data_manager.py

--- mgt/datamanagers/compound_word_data_manager.py
+++ mgt/datamanagers/compound_word_data_manager.py
@@ -7,7 +7,7 @@ from mgt.datamanagers.remi.to_midi_mapper import ToMidiMapper
 
 
 defaults = {
-    'transposition_steps': [0,1,2,3,4,5,6,7,8,9,10,11,12,-1,-2,-3,-4,-5,-6,-7,8,-9,-10,-11,-12],
+    'transposition_steps': [0],
     'map_tracks_to_instruments': {},
     'instrument_mapping': {}
 }
