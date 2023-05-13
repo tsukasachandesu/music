bdaa84883684b04b506a6a56d068675fc26a2de3
tsukasachandesu <44788261+tsukasachandesu@users.noreply.github.com>
2023-05-14 04:43:42 +0900

    Update compound_word_transformer_wrapper.py

--- mgt/models/compound_word_transformer/compound_word_transformer_wrapper.py
+++ mgt/models/compound_word_transformer/compound_word_transformer_wrapper.py
@@ -4,101 +4,20 @@
 # -- temperature -- #
 import numpy as np
 import torch
-from torch import nn, einsum
-from einops_exts import rearrange_with_anon_dims
-from einops import rearrange, reduce, repeat
+from torch import nn
 from x_transformers.x_transformers import AttentionLayers, default, AbsolutePositionalEmbedding, always
-import torch.nn.functional as F
+
 from mgt.models.compound_word_transformer.compound_transformer_embeddings import CompoundTransformerEmbeddings
 from mgt.models.utils import get_device
 
-def FeedForward(*, dim, mult = 4, dropout = 0.):
-    return nn.Sequential(
-        nn.LayerNorm(dim),
-        nn.Linear(dim, dim * mult),
-        nn.GELU(),
-        nn.Dropout(dropout),
-        nn.Linear(dim * mult, dim)
-    )
-
-class Attention(nn.Module):
-    def __init__(
-        self,
-        *,
-        dim,
-        dim_head = 64,
-        heads = 8,
-        dropout = 0.
-    ):
-        super().__init__()
-        self.scale = dim_head ** -0.5
-        self.heads = heads
-        inner_dim = dim_head * heads
-
-        self.dropout = nn.Dropout(dropout)
-        self.norm = nn.LayerNorm(dim)
-        self.to_qkv = nn.Linear(dim, inner_dim * 3, bias = False)
-        self.to_out = nn.Linear(inner_dim, dim, bias = False)
-
-    def forward(self, x):
-        h, device = self.heads, x.device
-
-        x = self.norm(x)
-        q, k, v = self.to_qkv(x).chunk(3, dim = -1)
-        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = h), (q, k, v))
-
-        q = q * self.scale
-        sim = einsum('b h i d, b h j d -> b h i j', q, k)
-
-        i, j = sim.shape[-2:]
-        mask_value = -torch.finfo(sim.dtype).max
-        mask = torch.ones((i, j), dtype = torch.bool, device = device).triu(j - i + 1)
-        sim = sim.masked_fill(mask, mask_value)
-
-        sim = sim - sim.amax(dim = -1, keepdim = True).detach()
-        attn = sim.softmax(dim = -1)
-        attn = self.dropout(attn)
-
-        out = einsum('b h i j, b h j d -> b h i d', attn, v)
-        out = rearrange(out, 'b h n d -> b n (h d)')
-        return self.to_out(out)
-
-class Transformer(nn.Module):
-    def __init__(
-        self,
-        *,
-        dim,
-        layers,
-        dim_head = 64,
-        heads = 8,
-        attn_dropout = 0.,
-        ff_dropout = 0.,
-        ff_mult = 4
-    ):
-        super().__init__()
-        self.layers = nn.ModuleList([])
-        for _ in range(layers):
-            self.layers.append(nn.ModuleList([
-                Attention(dim = dim, dim_head = dim_head, heads = heads, dropout = attn_dropout),
-                FeedForward(dim = dim, mult = ff_mult, dropout = ff_dropout)
-            ]))
-
-        self.norm = nn.LayerNorm(dim)
-
-    def forward(self, x):
-        for attn, ff in self.layers:
-            x = attn(x) + x
-            x = ff(x) + x
-
-        return self.norm(x)
-
 
 class VAETransformerEncoder(nn.Module):
-  def __init__(self, n_layer, n_head, d_model, d_ff, dropout, activation):
+  def __init__(self, n_layer, n_head, d_model, d_ff, dropout=0.1, activation='relu'):
     super(VAETransformerEncoder, self).__init__()
     self.n_layer = n_layer
     self.n_head = n_head
     self.d_model = d_model
+    self.d_ff = d_ff
     self.dropout = dropout
     self.activation = activation
 
@@ -111,11 +30,14 @@ class VAETransformerEncoder(nn.Module):
 
   def forward(self, x, padding_mask=None):
     out = self.tr_encoder(x, src_key_padding_mask=padding_mask)
-    return out
+    hidden_out = out[0, :, :]
+
+    return hidden_out
 
 def softmax_with_temperature(logits, temperature):
     probs = np.exp(logits / temperature) / np.sum(np.exp(logits / temperature))
     return probs
+
 
 def weighted_sampling(probs):
     probs /= sum(probs)
@@ -123,6 +45,7 @@ def weighted_sampling(probs):
     sorted_index = np.argsort(probs)[::-1]
     word = np.random.choice(sorted_index, size=1, p=sorted_probs)[0]
     return word
+
 
 # -- nucleus -- #
 def nucleus(probs, probability_treshold):
@@ -141,6 +64,7 @@ def nucleus(probs, probability_treshold):
     word = np.random.choice(candi_index, size=1, p=candi_probs)[0]
     return word
 
+
 def sampling(logit, probability_treshold=None, temperature=1.0):
     logit = logit.squeeze().cpu().detach().numpy()
     probs = softmax_with_temperature(logits=logit, temperature=temperature)
@@ -150,6 +74,7 @@ def sampling(logit, probability_treshold=None, temperature=1.0):
     else:
         cur_word = weighted_sampling(probs)
     return cur_word
+
 
 class CompoundWordTransformerWrapper(nn.Module):
     def __init__(
@@ -173,37 +98,10 @@ class CompoundWordTransformerWrapper(nn.Module):
                 512,  # Tempo
                 512,  # Instrument
                 512,  # Note Name
-                512,  # Octave
-                512,  # Duration
-                512  # Velocity
+                128,  # Octave
+                128,  # Duration
+                128  # Velocity
             ]
-        
-        self.depth_pos_emb = nn.Embedding(8, 512)
-        
-        self.spatial_transformer = Transformer(
-            dim = 512,
-            layers = 5,
-            dim_head = 64,
-            heads = 8,
-            attn_dropout = 0.1,
-            ff_dropout = 0.1,
-            ff_mult = 4
-        )
-        
-        self.spatial_start_token = nn.Parameter(torch.randn(512))
-
-        self.spatial_pos_emb = nn.Embedding(256, 512) 
-
-        self.depth_transformer = Transformer(
-            dim = 512,
-            layers = 5,
-            dim_head = 64,
-            heads = 8,
-            attn_dropout = 0.1,
-            ff_dropout = 0.1,
-            ff_mult = 4
-        )
-
 
         self.emb_sizes = emb_sizes
 
@@ -221,75 +119,37 @@ class CompoundWordTransformerWrapper(nn.Module):
         self.word_emb_octave = CompoundTransformerEmbeddings(self.num_tokens[5], self.emb_sizes[5])
         self.word_emb_duration = CompoundTransformerEmbeddings(self.num_tokens[6], self.emb_sizes[6])
         self.word_emb_velocity = CompoundTransformerEmbeddings(self.num_tokens[7], self.emb_sizes[7])
-        
-        self.proj_type1 = nn.Linear(4096, self.num_tokens[0])
-        self.proj_barbeat1 = nn.Linear(4096, self.num_tokens[1])
-        self.proj_tempo1 = nn.Sequential(
-            nn.Linear(4096, self.num_tokens[2])
-        )
-        self.proj_instrument1 =  nn.Sequential(
-            nn.Linear(4096, self.num_tokens[3])
-        )
-        self.proj_note_name1 = nn.Sequential(
-            nn.Linear(4096, self.num_tokens[4])
-        )
-        self.proj_octave1 = nn.Sequential(
-            nn.Linear(4096, self.num_tokens[5])
-        )
-         
-        self.proj_duration1 = nn.Sequential(
-            nn.Linear(4096, self.num_tokens[6])
-        )
-        
-        self.proj_velocity1 = nn.Sequential(
-            nn.Linear(4096, self.num_tokens[7])
-        )
-        
-        self.project_concat_type1 = nn.Linear(4096 + self.emb_sizes[0], 4096)
-        
+
         # individual output
         self.proj_type = nn.Linear(dim, self.num_tokens[0])
         self.proj_barbeat = nn.Linear(dim, self.num_tokens[1])
-        self.proj_tempo = nn.Sequential(
-            nn.Linear(dim, self.num_tokens[2])
-        )
-        self.proj_instrument =  nn.Sequential(
-            nn.Linear(dim, self.num_tokens[3])
-        )
-        self.proj_note_name = nn.Sequential(
-            nn.Linear(dim, self.num_tokens[4])
-        )
-        self.proj_octave = nn.Sequential(
-            nn.Linear(dim, self.num_tokens[5])
-        )
-         
-        self.proj_duration = nn.Sequential(
-            nn.Linear(dim, self.num_tokens[6])
-        )
-        
-        self.proj_velocity = nn.Sequential(
-            nn.Linear(dim, self.num_tokens[7])
-        )
-        
+        self.proj_tempo = nn.Linear(dim, self.num_tokens[2])
+        self.proj_instrument = nn.Linear(dim, self.num_tokens[3])
+        self.proj_note_name = nn.Linear(dim, self.num_tokens[4])
+        self.proj_octave = nn.Linear(dim, self.num_tokens[5])
+        self.proj_duration = nn.Linear(dim, self.num_tokens[6])
+        self.proj_velocity = nn.Linear(dim, self.num_tokens[7])
+
         # in_features is equal to dimension plus dimensions of the type embedding
         self.project_concat_type = nn.Linear(dim + self.emb_sizes[0], dim)
 
         self.compound_word_embedding_size = np.sum(emb_sizes)
 
-
-        
-
+        self.pos_emb = AbsolutePositionalEmbedding(self.compound_word_embedding_size, max_seq_len) if (
+                use_pos_emb and not attn_layers.has_pos_emb) else always(0)
         self.emb_dropout = nn.Dropout(emb_dropout)
 
         self.project_emb = nn.Linear(emb_dim, dim) if emb_dim != dim else nn.Identity()
         self.attn_layers = attn_layers
-        
         self.norm = nn.LayerNorm(dim)
 
-        self.in_linear = nn.Linear(4096, emb_dim)
-        self.in_linear1 = nn.Linear(4096, 1024)
-        self.pos_emb = AbsolutePositionalEmbedding(512, 256)
-        self.pos_emb1 = AbsolutePositionalEmbedding(512, 8) 
+        self.in_linear = nn.Linear(np.sum(self.emb_sizes), emb_dim)
+        self.in_linear1 = nn.Linear(4063, emb_dim)
+        self.in_linear2 = nn.Linear(2048, 1024)
+        
+        self.encoder = VAETransformerEncoder(
+            3, 8, 1024, 2048, 0.1, 'relu'
+        )
 
         self.init_()
 
@@ -324,16 +184,16 @@ class CompoundWordTransformerWrapper(nn.Module):
 
         # concat
         y_concat_type = torch.cat([h, tf_skip_type], dim=-1)
-        y_ = self.project_concat_type1(y_concat_type)
+        y_ = self.project_concat_type(y_concat_type)
 
         # project other
-        proj_barbeat = self.proj_barbeat1(y_)
-        proj_tempo = self.proj_tempo1(y_)
-        proj_instrument = self.proj_instrument1(y_)
-        proj_note_name = self.proj_note_name1(y_)
-        proj_octave = self.proj_octave1(y_)
-        proj_duration = self.proj_duration1(y_)
-        proj_velocity = self.proj_velocity1(y_)
+        proj_barbeat = self.proj_barbeat(y_)
+        proj_tempo = self.proj_tempo(y_)
+        proj_instrument = self.proj_instrument(y_)
+        proj_note_name = self.proj_note_name(y_)
+        proj_octave = self.proj_octave(y_)
+        proj_duration = self.proj_duration(y_)
+        proj_velocity = self.proj_velocity(y_)
 
         # sampling gen_cond
         cur_word_barbeat = sampling(
@@ -388,21 +248,18 @@ class CompoundWordTransformerWrapper(nn.Module):
                        h,
                        target
                        ):
-
         tf_skip_type = self.word_emb_type(target[..., 0])
 
         y_concat_type = torch.cat([h, tf_skip_type], dim=-1)
-        y_ = self.project_concat_type1(y_concat_type)
+        y_ = self.project_concat_type(y_concat_type)
 
-
-        proj_barbeat = self.proj_barbeat1(y_)
-
-        proj_tempo = self.proj_tempo1(y_)
-        proj_instrument = self.proj_instrument1(y_)
-        proj_note_name = self.proj_note_name1(y_)
-        proj_octave = self.proj_octave1(y_)
-        proj_duration = self.proj_duration1(y_)
-        proj_velocity = self.proj_velocity1(y_)
+        proj_barbeat = self.proj_barbeat(y_)
+        proj_tempo = self.proj_tempo(y_)
+        proj_instrument = self.proj_instrument(y_)
+        proj_note_name = self.proj_note_name(y_)
+        proj_octave = self.proj_octave(y_)
+        proj_duration = self.proj_duration(y_)
+        proj_velocity = self.proj_velocity(y_)
 
         return proj_barbeat, proj_tempo, proj_instrument, proj_note_name, proj_octave, proj_duration, proj_velocity
 
@@ -433,41 +290,26 @@ class CompoundWordTransformerWrapper(nn.Module):
                 emb_duration,
                 emb_velocity
             ], dim=2)
-                                                
-        device=embs.device
-        devi=embs.shape
+                         
+
+        emb_linear = self.in_linear1(embs)
         
-        embs = rearrange(embs, 'b s d f -> (b s) d f')
-        tokens_with_depth_pos = embs + self.pos_emb1(embs)
-        tokens_with_depth_pos = rearrange(tokens_with_depth_pos, '(b s) d f -> b s d f', b = devi[0])
-
-        # spatial tokens is tokens with depth pos reduced along depth dimension + spatial positions
-
-        spatial_tokens = reduce(tokens_with_depth_pos, 'b s d f -> b s f', 'sum') 
-        spatial_tokens = spatial_tokens + self.pos_emb(spatial_tokens)
+        h = rearrange(emb_linear, 'b d f -> b s d f', d = 17)
+        h = rearrange(h, 'b s d f -> (b s) d f')
+        h = h + self.pos_emb(h)
+        h = self.encoder(h)
+        h = h.repeat_interleave(15)
+        embs = torch.cat([embs,h], dim=-1)
+        emb_linear = self.in_linear2(embs)
         
-        spatial_tokens = torch.cat((
-            repeat(self.spatial_start_token, 'f -> b 1 f', b = devi[0]),
-            spatial_tokens
-        ), dim = -2)        
+        x = emb_linear + self.pos_emb(emb_linear)
+        x = self.emb_dropout(x)
+        x = self.project_emb(x)
 
-        spatial_tokens = self.spatial_transformer(spatial_tokens)
+        if not self.training:
+            x.squeeze(0)
 
-        spatial_tokens = rearrange(spatial_tokens, 'b s f -> b s 1 f')
+        x, intermediates = self.attn_layers(x, mask=mask, return_hiddens=True, **kwargs)
+        x = self.norm(x)
 
-        # spatial tokens become the start tokens of the depth dimension
-
-        tokens_with_depth_pos = F.pad(tokens_with_depth_pos, (0, 0, 0, 0, 0, 1), value = 0.)
-
-        depth_tokens = torch.cat((spatial_tokens, tokens_with_depth_pos), dim = -2)
-
-        depth_tokens = rearrange(depth_tokens, '... n d -> (...) n d')
-
-        depth_tokens = self.depth_transformer(depth_tokens)
-
-        x = rearrange(depth_tokens, '(b s) d f -> b s d f', b = devi[0])
-        x = x[:,:-1,:-1,:]
-        p = x.shape
-        x = x.view(p[0], p[1], -1)
-
-        return x, self.proj_type1(x)
+        return x, self.proj_type(x)
