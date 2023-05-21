08a26a872c89adee58bd87f81ab86d36febdd404
tsukasachandesu <44788261+tsukasachandesu@users.noreply.github.com>
2023-05-21 13:18:39 +0900

    Update compound_word_transformer_wrapper.py

--- mgt/models/compound_word_transformer/compound_word_transformer_wrapper.py
+++ mgt/models/compound_word_transformer/compound_word_transformer_wrapper.py
@@ -12,9 +12,66 @@ import torch.nn.functional as F
 from mgt.models.compound_word_transformer.compound_transformer_embeddings import CompoundTransformerEmbeddings
 from mgt.models.utils import get_device
 
+# token shift, from Peng et al of RWKV
+
+def token_shift(t):
+    t, t_shift = t.chunk(2, dim = -1)
+    t_shift = F.pad(t_shift, (0, 0, 1, -1))
+    return torch.cat((t, t_shift), dim = -1)
+
+# positional bias
+
+class Alibi(nn.Module):
+    def __init__(self, heads, **kwargs):
+        super().__init__()
+        self.heads = heads
+        slopes = torch.Tensor(self._get_slopes(heads))
+        slopes = rearrange(slopes, 'h -> h 1 1')
+        self.register_buffer('slopes', slopes, persistent = False)
+        self.register_buffer('bias', None, persistent = False)
+
+    @staticmethod
+    def _get_slopes(heads):
+        def get_slopes_power_of_2(n):
+            start = (2**(-2**-(math.log2(n)-3)))
+            ratio = start
+            return [start*ratio**i for i in range(n)]
+
+        if math.log2(heads).is_integer():
+            return get_slopes_power_of_2(heads)
+
+        closest_power_of_2 = 2 ** math.floor(math.log2(heads))
+        return get_slopes_power_of_2(closest_power_of_2) + get_slopes_power_of_2(2 * closest_power_of_2)[0::2][:heads-closest_power_of_2]
+
+    def forward(self, i, j, device):
+        if exists(self.bias) and self.bias.shape[-1] >= j:
+            return self.bias[..., :j]
+
+        bias = torch.arange(j, device = device)
+        bias = rearrange(bias, 'j -> 1 1 j')
+        bias = bias * self.slopes
+
+        self.register_buffer('bias', bias, persistent = False)
+        return self.bias
+
+# norm
+
+class RMSNorm(nn.Module):
+    def __init__(self, dim, eps = 1e-8):
+        super().__init__()
+        self.scale = dim ** -0.5
+        self.eps = eps
+        self.g = nn.Parameter(torch.ones(dim))
+
+    def forward(self, x):
+        norm = torch.norm(x, dim = -1, keepdim = True) * self.scale
+        return x / norm.clamp(min = self.eps) * self.g
+
+# helper classes
+
 def FeedForward(*, dim, mult = 4, dropout = 0.):
     return nn.Sequential(
-        nn.LayerNorm(dim),
+        RMSNorm(dim),
         nn.Linear(dim, dim * mult),
         nn.GELU(),
         nn.Dropout(dropout),
@@ -28,38 +85,35 @@ class Attention(nn.Module):
         dim,
         dim_head = 64,
         heads = 8,
-        dropout = 0.
+        dropout = 0.,
+        flash = False
     ):
         super().__init__()
         self.scale = dim_head ** -0.5
         self.heads = heads
         inner_dim = dim_head * heads
 
+        self.attend = Attend(
+            causal = True,
+            flash = flash,
+            dropout = dropout
+        )
+
         self.dropout = nn.Dropout(dropout)
-        self.norm = nn.LayerNorm(dim)
-        self.to_qkv = nn.Linear(dim, inner_dim * 3, bias = False)
+        self.norm = RMSNorm(dim)
+        self.to_q = nn.Linear(dim, inner_dim, bias = False)
+        self.to_kv = nn.Linear(dim, dim_head * 2, bias = False)
         self.to_out = nn.Linear(inner_dim, dim, bias = False)
 
-    def forward(self, x):
+    def forward(self, x, attn_bias = None):
         h, device = self.heads, x.device
 
         x = self.norm(x)
-        q, k, v = self.to_qkv(x).chunk(3, dim = -1)
-        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = h), (q, k, v))
+        q, k, v = (self.to_q(x), *self.to_kv(x).chunk(2, dim = -1))
+        q = rearrange(q, 'b n (h d) -> b h n d', h = h)
 
-        q = q * self.scale
-        sim = einsum('b h i d, b h j d -> b h i j', q, k)
+        out = self.attend(q, k, v, attn_bias = attn_bias)
 
-        i, j = sim.shape[-2:]
-        mask_value = -torch.finfo(sim.dtype).max
-        mask = torch.ones((i, j), dtype = torch.bool, device = device).triu(j - i + 1)
-        sim = sim.masked_fill(mask, mask_value)
-
-        sim = sim - sim.amax(dim = -1, keepdim = True).detach()
-        attn = sim.softmax(dim = -1)
-        attn = self.dropout(attn)
-
-        out = einsum('b h i j, b h j d -> b h i d', attn, v)
         out = rearrange(out, 'b h n d -> b n (h d)')
         return self.to_out(out)
 
@@ -73,22 +127,29 @@ class Transformer(nn.Module):
         heads = 8,
         attn_dropout = 0.,
         ff_dropout = 0.,
-        ff_mult = 4
+        ff_mult = 4,
+        rel_pos_bias = True,
+        flash_attn = False
     ):
         super().__init__()
+        self.alibi = Alibi(heads = heads) if rel_pos_bias else None
         self.layers = nn.ModuleList([])
+
         for _ in range(layers):
             self.layers.append(nn.ModuleList([
-                Attention(dim = dim, dim_head = dim_head, heads = heads, dropout = attn_dropout),
+                Attention(dim = dim, dim_head = dim_head, heads = heads, dropout = attn_dropout, flash = flash_attn),
                 FeedForward(dim = dim, mult = ff_mult, dropout = ff_dropout)
             ]))
 
-        self.norm = nn.LayerNorm(dim)
+        self.norm = RMSNorm(dim)
 
     def forward(self, x):
+        n = x.shape[-2]
+        attn_bias = self.alibi(n, n, device = x.device) if exists(self.alibi) else None
+
         for attn, ff in self.layers:
-            x = attn(x) + x
-            x = ff(x) + x
+            x = attn(token_shift(x), attn_bias = attn_bias) + x
+            x = ff(token_shift(x)) + x
 
         return self.norm(x)
 
