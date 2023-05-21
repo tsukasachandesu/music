41395c1e973d14ddee962f36eab135aae156f3fe
tsukasachandesu <44788261+tsukasachandesu@users.noreply.github.com>
2023-05-21 13:52:44 +0900

    Update compound_word_transformer_wrapper.py

--- mgt/models/compound_word_transformer/compound_word_transformer_wrapper.py
+++ mgt/models/compound_word_transformer/compound_word_transformer_wrapper.py
@@ -6,13 +6,32 @@ import torch
 from torch import nn, einsum
 from einops_exts import rearrange_with_anon_dims
 from einops import rearrange, reduce, repeat, pack, unpack
-from x_transformers.x_transformers import AbsolutePositionalEmbedding
+
 import torch.nn.functional as F
 from mgt.models.compound_word_transformer.compound_transformer_embeddings import CompoundTransformerEmbeddings
 from mgt.models.utils import get_device
 
 from mgt.models.compound_word_transformer.encoder import Attend
 from einops.layers.torch import Rearrange
+
+class AbsolutePositionalEmbedding(nn.Module):
+    def __init__(self, dim, max_seq_len, l2norm_embed = False):
+        super().__init__()
+        self.scale = dim ** -0.5 if not l2norm_embed else 1.
+        self.max_seq_len = max_seq_len
+        self.l2norm_embed = l2norm_embed
+        self.emb = nn.Embedding(max_seq_len, dim)
+
+    def forward(self, x, pos = None):
+        seq_len, device = x.shape[1], x.device
+        assert seq_len <= self.max_seq_len, f'you are passing in a sequence length of {seq_len} but your absolute positional embedding has a max sequence length of {self.max_seq_len}'
+
+        if not exists(pos):
+            pos = torch.arange(seq_len, device = device)
+
+        pos_emb = self.emb(pos)
+        pos_emb = pos_emb * self.scale
+        return l2norm(pos_emb) if self.l2norm_embed else pos_emb
 
 def exists(val):
     return val is not None
