aa4065307563f542285f5bb78d75778e4c78cfb5
tsukasachandesu <44788261+tsukasachandesu@users.noreply.github.com>
2023-05-26 05:14:01 +0900

    Update 0.py

--- 0.py
+++ 0.py
@@ -12,6 +12,8 @@ from mgt.models.compound_word_transformer.compound_word_autoregressive_wrapper i
 from mgt.models.compound_word_transformer.compound_word_transformer_utils import COMPOUND_WORD_BAR, get_batch
 from mgt.models.compound_word_transformer.compound_word_transformer_wrapper import CompoundWordTransformerWrapper
 from mgt.models.utils import get_device
+from mgt.datamanagers.compound_word_data_manager import CompoundWordDataManager
+
 
 import random
 import tqdm
@@ -24,9 +26,7 @@ import torch.nn.functional as F
 from torch.utils.data import DataLoader, Dataset
 import argparse
 
-datamanager = RemiDataManager(
-    efficient_remi_config=EfficientRemiConfig(enabled=True, remove_velocity=True)
-)
+datamanager = CompoundWordDataManager()
 
 class Dataset(Dataset):
     def __init__(self, data, max_length=1024):
@@ -77,25 +77,49 @@ yes = None
 
 # instantiate GPT-like decoder model
 
-model = RecurrentMemoryTransformer(
-    num_tokens=self.dictionary.size(),
-    dim = 512,
-    depth = 6,
-    dim_head = 64,
-    heads = 8,
-    seq_len = 1024,
-    use_flash_attn = True,
-    num_memory_tokens = 128,
-    use_xl_memories = True,
-    causal=True,
-    ignore_index=0,
-    xl_mem_len = 512
-)
+defaults = {
+    'num_tokens': [
+        4,    # Type
+        17,   # Bar / Beat
+        6912,  # Tempo
+        6912,  # Instrument
+        6912,   # Note name
+        6912,    # Octave
+        6912,   # Duration
+        6912    # Velocity
+    ],
+    'emb_sizes': [
+        32,   # Type
+        96,   # Bar / Beat
+        512,  # Tempo
+        512,  # Instrument
+        512,  # Note Name
+        512,  # Octave
+        512,  # Duration
+        512   # Velocity
+    ],
+    'max_sequence_length': 1024,
+    'learning_rate': 1e-4,
+    'dropout': 0.1,
+    'dim': 512,
+    'depth': 24,
+    'heads': 8
+}
 
-model = RecurrentMemoryTransformerWrapper(model)
-
-model.cuda()
-
+model = CompoundWordAutoregressiveWrapper(CompoundWordTransformerWrapper(
+    num_tokens=defaults["num_tokens"],
+    emb_sizes=defaults["emb_sizes"],
+    max_seq_len=["max_sequence_length"],
+    attn_layers=Decoder(
+        dim=defaults["dim"],
+        depth=defaults["depth"],
+        heads=defaults["heads"],
+        ff_glu = True,
+        ff_swish = True,
+        rotary_xpos = True,
+        attn_dropout=defaults["dropout"],  
+        ff_dropout=defaults["dropout"],  
+    ))).cuda()
 
 # setup deepspeed
 data_train = DataHelper.load('/content/drive/MyDrive/b.dat')
@@ -111,7 +135,7 @@ for _ in range(EPOCHS):
     for i, data in enumerate(trainloader):
         model_engine.train()
         data = data.to(model_engine.local_rank)
-        loss = model_engine(data, memory_replay_backprop=True)
+        loss = model_engine(data)
         model_engine.backward(loss)
         torch.nn.utils.clip_grad_norm_(model_engine.parameters(), 0.5)
         model_engine.step()
