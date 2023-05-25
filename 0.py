da98af64644ab19d204718a5296e6819e8995e51
tsukasachandesu <44788261+tsukasachandesu@users.noreply.github.com>
2023-05-26 07:14:25 +0900

    Update 0.py

--- 0.py
+++ 0.py
@@ -1,17 +1,24 @@
+from __future__ import annotations
 import pickle
 import deepspeed
 
-from recurrent_memory_transformer_pytorch import RecurrentMemoryTransformer, RecurrentMemoryTransformerWrapper
+import time
+import numpy as np
+import torch
+from x_transformers import Decoder
 
-from mgt.datamanagers.remi_data_manager import RemiDataManager
+from mgt.models import utils
+from mgt.models.compound_word_transformer.compound_word_autoregressive_wrapper import CompoundWordAutoregressiveWrapper
+from mgt.models.compound_word_transformer.compound_word_transformer_utils import COMPOUND_WORD_BAR, get_batch
+from mgt.models.compound_word_transformer.compound_word_transformer_wrapper import CompoundWordTransformerWrapper
+from mgt.models.utils import get_device
+from mgt.datamanagers.compound_word_data_manager import CompoundWordDataManager
 from mgt.datamanagers.data_helper import DataHelper
-from mgt.datamanagers.remi.efficient_remi_config import EfficientRemiConfig
 
 import random
 import tqdm
 import gzip
-import numpy as np
-import torch
+
 import torch.optim as optim
 from einops import rearrange
 from torch import einsum, nn
@@ -19,9 +26,23 @@ import torch.nn.functional as F
 from torch.utils.data import DataLoader, Dataset
 import argparse
 
-datamanager = RemiDataManager(
-    efficient_remi_config=EfficientRemiConfig(enabled=True, remove_velocity=True)
-)
+datamanager = CompoundWordDataManager()
+
+from typing import List
+import random
+
+COMPOUND_WORD_PADDING = [0, 0, 0, 0, 0, 0, 0, 0]
+COMPOUND_WORD_BAR = [2, 0, 0, 0, 0, 0, 0, 0]
+
+def pad(array: np.ndarray, max_sequence_length: int, padding_compound_word: np.ndarray = None) -> np.ndarray:
+    if padding_compound_word is None:
+        padding_compound_word = COMPOUND_WORD_PADDING
+    if len(array) >= max_sequence_length:
+        return array[:max_sequence_length]
+    else:
+        number_of_padding_elements_to_add = max_sequence_length - len(array)
+        padding = np.tile(padding_compound_word, (number_of_padding_elements_to_add, 1))
+        return np.vstack((array, padding))
 
 class Dataset(Dataset):
     def __init__(self, data, max_length=1024):
@@ -29,20 +50,19 @@ class Dataset(Dataset):
         self.max_length = max_length
         
     def __len__(self):
-        return 500
+        return 512
 
     def __getitem__(self, idx):
         song_index = random.randint(0, len(self.data) - 1)
         if len(self.data[song_index]) <= self.max_length:
-          starting_index = random.randint(0, len(self.data[song_index]) - 1)
-          padded_song = list(np.repeat(0, self.max_length))+self.data[song_index]
-          a = padded_song[0:self.max_length]
+          padded_song = pad(self.data[song_index], self.max_length)
+          a = padded_song
         else:
           starting_index = random.randint(0, len(self.data[song_index]) - self.max_length)
           a = self.data[song_index][starting_index: starting_index + self.max_length]
-
+            
         return torch.tensor(a).long()
-  
+
 def add_argument():
     parser=argparse.ArgumentParser(description='enwik8')
 
@@ -50,9 +70,9 @@ def add_argument():
                         help='use CPU in case there\'s no GPU support')
     parser.add_argument('--use_ema', default=False, action='store_true',
                         help='whether use exponential moving average')
-    parser.add_argument('-b', '--batch_size', default=4, type=int,
+    parser.add_argument('-b', '--batch_size', default=2, type=int,
                         help='mini-batch size (default: 32)')
-    parser.add_argument('-e', '--epochs', default=30, type=int,
+    parser.add_argument('-e', '--epochs', default=4, type=int,
                         help='number of total epochs (default: 30)')
     parser.add_argument('--local_rank', type=int, default=-1,
                        help='local rank passed from distributed launcher')
@@ -63,39 +83,61 @@ def add_argument():
 
 # constants
 
-EPOCHS = 5
-GRADIENT_ACCUMULATE_EVERY = 4
+EPOCHS = 4
+GRADIENT_ACCUMULATE_EVERY = 1
 GENERATE_EVERY = 1800
 GENERATE_LENGTH = 1024
-SEQ_LEN = 1024
 yes = None
 
 # instantiate GPT-like decoder model
 
-model = RecurrentMemoryTransformer(
-    num_tokens=self.dictionary.size(),
-    dim = 512,
-    depth = 6,
-    dim_head = 64,
-    heads = 8,
-    seq_len = 1024,
-    use_flash_attn = True,
-    num_memory_tokens = 128,
-    use_xl_memories = True,
-    causal=True,
-    ignore_index=0,
-    xl_mem_len = 512
-)
+defaults = {
+    'num_tokens': [
+        4,    # Type
+        17,   # Bar / Beat
+        6912,  # Tempo
+        6912,  # Instrument
+        6912,   # Note name
+        6912,    # Octave
+        6912,   # Duration
+        6912    # Velocity
+    ],
+    'emb_sizes': [
+        32,   # Type
+        96,   # Bar / Beat
+        512,  # Tempo
+        512,  # Instrument
+        512,  # Note Name
+        512,  # Octave
+        512,  # Duration
+        512   # Velocity
+    ],
+    'max_sequence_length': 1024,
+    'learning_rate': 1e-4,
+    'dropout': 0.1,
+    'dim': 512,
+    'depth': 24,
+    'heads': 8
+}
 
-model = RecurrentMemoryTransformerWrapper(model)
-
-model.cuda()
-
+model = CompoundWordAutoregressiveWrapper(CompoundWordTransformerWrapper(
+    num_tokens=defaults["num_tokens"],
+    emb_sizes=defaults["emb_sizes"],
+    max_seq_len=defaults["max_sequence_length"],
+    attn_layers=Decoder(
+        dim=defaults["dim"],
+        depth=defaults["depth"],
+        heads=defaults["heads"],
+        ff_glu = True,
+        ff_swish = True,
+        rotary_xpos = True,
+        attn_dropout=defaults["dropout"],  
+        ff_dropout=defaults["dropout"],  
+    ))).cuda()
 
 # setup deepspeed
-data_train = DataHelper.load('/content/drive/MyDrive/b.dat')
-data_train = data_train.data
-train_dataset = Dataset(data_train, 8192)
+data_train = DataHelper.load('/content/drive/MyDrive/1data')
+train_dataset = Dataset(data_train.data)
 
 cmd_args = add_argument()
 model_engine, optimizer, trainloader, _ = deepspeed.initialize(args=cmd_args, model=model, model_parameters=model.parameters(), training_data=train_dataset)
@@ -106,17 +148,13 @@ for _ in range(EPOCHS):
     for i, data in enumerate(trainloader):
         model_engine.train()
         data = data.to(model_engine.local_rank)
-        loss = model_engine(data, memory_replay_backprop=True)
+        loss = model_engine.train_step(data)
+        loss = sum(loss) / len(loss)
         model_engine.backward(loss)
         torch.nn.utils.clip_grad_norm_(model_engine.parameters(), 0.5)
         model_engine.step()
         print(loss.item())
 
-model.eval()          
-prompt = [0]
-initial = torch.tensor([prompt]).long().cuda() 
-sample = model.generate(initial,1024)
-sample = sample.cpu().detach().numpy()[0]
-midi = datamanager.to_midi(sample)
-midi.save("1.midi")
-model_engine.save_checkpoint("/content/3")
+model.eval()    
+if yes:
+    model_engine.save_checkpoint("/content/3")
