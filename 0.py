2b7a292270bc853063ee22db71f275f50cad49b7
tsukasachandesu <44788261+tsukasachandesu@users.noreply.github.com>
2023-05-26 05:29:57 +0900

    Update 0.py

--- 0.py
+++ 0.py
@@ -28,26 +28,6 @@ import argparse
 
 datamanager = CompoundWordDataManager()
 
-class Dataset(Dataset):
-    def __init__(self, data, max_length=1024):
-        self.data = data
-        self.max_length = max_length
-        
-    def __len__(self):
-        return 500
-
-    def __getitem__(self, idx):
-        song_index = random.randint(0, len(self.data) - 1)
-        if len(self.data[song_index]) <= self.max_length:
-          starting_index = random.randint(0, len(self.data[song_index]) - 1)
-          padded_song = list(np.repeat(0, self.max_length))+self.data[song_index]
-          a = padded_song[0:self.max_length]
-        else:
-          starting_index = random.randint(0, len(self.data[song_index]) - self.max_length)
-          a = self.data[song_index][starting_index: starting_index + self.max_length]
-
-        return torch.tensor(a).long()
-  
 def add_argument():
     parser=argparse.ArgumentParser(description='enwik8')
 
@@ -69,10 +49,9 @@ def add_argument():
 # constants
 
 EPOCHS = 5
-GRADIENT_ACCUMULATE_EVERY = 4
+GRADIENT_ACCUMULATE_EVERY = 0
 GENERATE_EVERY = 1800
 GENERATE_LENGTH = 1024
-SEQ_LEN = 1024
 yes = None
 
 # instantiate GPT-like decoder model
@@ -109,7 +88,7 @@ defaults = {
 model = CompoundWordAutoregressiveWrapper(CompoundWordTransformerWrapper(
     num_tokens=defaults["num_tokens"],
     emb_sizes=defaults["emb_sizes"],
-    max_seq_len=["max_sequence_length"],
+    max_seq_len=defaults["max_sequence_length"],
     attn_layers=Decoder(
         dim=defaults["dim"],
         depth=defaults["depth"],
@@ -123,8 +102,7 @@ model = CompoundWordAutoregressiveWrapper(CompoundWordTransformerWrapper(
 
 # setup deepspeed
 data_train = DataHelper.load('/content/drive/MyDrive/b.dat')
-data_train = data_train.data
-train_dataset = Dataset(data_train, 8192)
+train_dataset = get_batch(data_train.data, batch_size=4, max_sequence_length=defaults["max_sequence_length"])
 
 cmd_args = add_argument()
 model_engine, optimizer, trainloader, _ = deepspeed.initialize(args=cmd_args, model=model, model_parameters=model.parameters(), training_data=train_dataset)
@@ -134,18 +112,14 @@ if yes:
 for _ in range(EPOCHS):
     for i, data in enumerate(trainloader):
         model_engine.train()
+        data = torch.tensor(np.array(data)).long().cuda()
         data = data.to(model_engine.local_rank)
-        loss = model_engine(data)
+        loss = model_engine.train_step(data)
+        loss = sum(loss) / len(loss)
         model_engine.backward(loss)
         torch.nn.utils.clip_grad_norm_(model_engine.parameters(), 0.5)
         model_engine.step()
         print(loss.item())
 
 model.eval()          
-prompt = [0]
-initial = torch.tensor([prompt]).long().cuda() 
-sample = model.generate(initial,1024)
-sample = sample.cpu().detach().numpy()[0]
-midi = datamanager.to_midi(sample)
-midi.save("1.midi")
 model_engine.save_checkpoint("/content/3")
