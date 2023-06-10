4dfbd4a5c186acebd822316bfcf29729b369665f
tsukasachandesu <44788261+tsukasachandesu@users.noreply.github.com>
2023-06-10 19:34:31 +0900

    Update compound_word_data_manager.py

--- mgt/datamanagers/compound_word_data_manager.py
+++ mgt/datamanagers/compound_word_data_manager.py
@@ -164,7 +164,8 @@ class CompoundWordDataManager(DataManager):
                 q.append([2,0,0,0,0,0,0,0])
             else:
                 q.append([2,i[1],0,0,0,0,0,0])
-                q.append([3,i[1],0,0,*inverse_dic[int(i[2]-1)],31])
+                if i[2] != 0:
+                    q.append([3,i[1],0,0,*inverse_dic[int(i[2]-1)],31])
                 
         remi = self.compound_word_mapper.map_to_remi(q)
         return MidiToolkitWrapper(self.to_midi_mapper.to_midi(remi))
