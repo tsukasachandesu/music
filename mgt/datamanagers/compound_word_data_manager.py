1dc5356863e611b386dc275163dff20aa87d2fa2
tsukasachandesu <44788261+tsukasachandesu@users.noreply.github.com>
2023-06-10 16:21:58 +0900

    Update compound_word_data_manager.py

--- mgt/datamanagers/compound_word_data_manager.py
+++ mgt/datamanagers/compound_word_data_manager.py
@@ -128,7 +128,7 @@ class CompoundWordDataManager(DataManager):
                                 r = r + 1
                             if i[7] != 12*9*64:
                                 r = r + 1
-                        pq.append(i[0],i[1],i[2],i[3],i[4],i[5],i[6],i[7],r)
+                        pq.append([i[0],i[1],i[2],i[3],i[4],i[5],i[6],i[7],r])
 
 
                     print(f'Extracted {len(p)} compound words.')
