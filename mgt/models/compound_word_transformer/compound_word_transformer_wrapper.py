a91f913d768dead6babbb9e42c4fe03f5462be86
tsukasachandesu <44788261+tsukasachandesu@users.noreply.github.com>
2023-06-11 22:23:49 +0900

    Update compound_word_transformer_wrapper.py

--- mgt/models/compound_word_transformer/compound_word_transformer_wrapper.py
+++ mgt/models/compound_word_transformer/compound_word_transformer_wrapper.py
@@ -204,7 +204,7 @@ class CompoundWordTransformerWrapper(nn.Module):
         self.norm = nn.LayerNorm(512)
         self.in_linear1 = nn.Linear(512*6+96+32+126, 512)
         
-        self.in_linear = nn.Linear(4, 126)
+        self.in_linear = nn.Linear(1, 126)
 
         self.init_()
 
@@ -366,7 +366,7 @@ class CompoundWordTransformerWrapper(nn.Module):
         emb_duration = self.word_emb_duration(x[..., 6])
         emb_velocity = self.word_emb_velocity(x[..., 7])
        
-        emb = self.in_linear(x[..., 8:])
+        emb = self.in_linear(x[..., -1])
         
         embs1 = torch.cat(
             [
