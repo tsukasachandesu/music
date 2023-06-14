6c4671d77fe63de297fdb60e4c4266c4fb30d7b1
tsukasachandesu <44788261+tsukasachandesu@users.noreply.github.com>
2023-06-14 22:31:02 +0900

    Update compound_word_data_manager.py

--- mgt/datamanagers/compound_word_data_manager.py
+++ mgt/datamanagers/compound_word_data_manager.py
@@ -229,17 +229,18 @@ class CompoundWordDataManager(DataManager):
                             
                     dic2 = {(i, j): index for index, (i, j) in enumerate((i, j) for j in range(108) for i in range(2))}
                     dic1 = {(i, j): index for index, (i, j) in enumerate((i, j) for j in range(9) for i in range(12))}
-
+                    print(pm)
                     
                     for i in range(len(ppqq)):
                         for jj in range(6):
                             j = jj + 1 
                             if ppqq[i][j] != 6913 and ppqq[i][j] != 0:
                                 if inverse_dic[ppqq[i][j]][2] > 1:
-                                    pm[dic1.get(inverse_dic[ppqq[i][j]][0],inverse_dic[ppqq[i][j]][1])][i+inverse_dic[ppqq[i][j]][2]-1] = 2
-                                    pm[dic1.get(inverse_dic[ppqq[i][j]][0],inverse_dic[ppqq[i][j]][1])][i:i+inverse_dic[ppqq[i][j]][2]-1] += 1
+                                    print("a")
+                                    pm[dic1.get(inverse_dic[ppqq[i][j]-1][0],inverse_dic[ppqq[i][j]][1]-1)][i+inverse_dic[ppqq[i][j]-1][2]-1] = 2
+                                    pm[dic1.get(inverse_dic[ppqq[i][j]-1][0],inverse_dic[ppqq[i][j]][1]-1)][i:i+inverse_dic[ppqq[i][j]-1][2]-1] += 1
                                 if inverse_dic[ppqq[i][j]][2] == 1:
-                                    pm[dic1.get(inverse_dic[ppqq[i][j]][0],inverse_dic[ppqq[i][j]][1])][i+inverse_dic[ppqq[i][j]][2]-1] = 2
+                                    pm[dic1.get(inverse_dic[ppqq[i][j]-1][0],inverse_dic[ppqq[i][j]][1]-1)][i+inverse_dic[ppqq[i][j]-1][2]-1] = 2
                     print(pm)
                     zzzz = []
                     for i in range(len(ppqq)):
