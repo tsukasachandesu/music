aec43256ab98a5464a2971442cf9b57a6b1e8660
tsukasachandesu <44788261+tsukasachandesu@users.noreply.github.com>
2023-06-17 15:04:04 +0900

    Update compound_word_autoregressive_wrapper.py

--- mgt/models/compound_word_transformer/compound_word_autoregressive_wrapper.py
+++ mgt/models/compound_word_transformer/compound_word_autoregressive_wrapper.py
@@ -79,7 +79,7 @@ class CompoundWordAutoregressiveWrapper(nn.Module):
 
         h = self.net.forward_hidden(xi,**kwargs)
         
-        proj_type, proj_barbeat, proj_tempo, proj_instrument, proj_note_name, proj_octave, proj_duration = self.net.forward_output(h)
+        proj_type, proj_barbeat, proj_tempo, proj_instrument, proj_note_name, proj_octave, proj_duration,proj_duration1,proj_duration2,proj_duration3,proj_duration4,proj_duration5 = self.net.forward_output(h)
 
         type_loss = calculate_loss(proj_type, target[..., 0], type_mask(target))
         barbeat_loss = calculate_loss(proj_barbeat, target[..., 1], type_mask(target))
@@ -89,4 +89,10 @@ class CompoundWordAutoregressiveWrapper(nn.Module):
         octave_loss = calculate_loss(proj_octave, target[..., 5], type_mask(target))
         duration_loss = calculate_loss(proj_duration, target[..., 6], type_mask(target))
         
-        return type_loss, barbeat_loss, tempo_loss, instrument_loss, note_name_loss, octave_loss, duration_loss
+        duration_loss1 = calculate_loss1(proj_duration1, target[..., 7], type_mask(target))
+        duration_loss2 = calculate_loss1(proj_duration2, target[..., 8], type_mask(target))
+        duration_loss3 = calculate_loss1(proj_duration3, target[..., 9], type_mask(target))
+        duration_loss4 = calculate_loss1(proj_duration4, target[..., 10], type_mask(target))
+        duration_loss5 = calculate_loss1(proj_duration5, target[..., 11], type_mask(target))
+        
+        return type_loss, barbeat_loss, tempo_loss, instrument_loss, note_name_loss, octave_loss, duration_loss,duration_loss1,duration_loss2,duration_loss3,duration_loss4,duration_loss5
