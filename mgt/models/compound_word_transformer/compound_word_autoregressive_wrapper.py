c12fc3216255408c78ef4eb5c7eebb051066c94a
tsukasachandesu <44788261+tsukasachandesu@users.noreply.github.com>
2023-06-25 13:51:53 +0900

    Update compound_word_autoregressive_wrapper.py

--- mgt/models/compound_word_transformer/compound_word_autoregressive_wrapper.py
+++ mgt/models/compound_word_transformer/compound_word_autoregressive_wrapper.py
@@ -17,7 +17,7 @@ def calculate_loss1(predicted, target, loss_mask):
         return 0
 
     loss = F.mse_loss(predicted[:, ...], target, reduction = 'none')
-    loss = loss * loss_mask.unsqueeze(-1)
+    loss = loss * loss_mask
     loss = torch.sum(loss) / trainable_values
 
     return loss
@@ -124,10 +124,8 @@ class CompoundWordAutoregressiveWrapper(nn.Module):
         proj_duration1 = torch.softmax(proj_duration, dim=0)
         
         f = proj_barbeat1 + proj_tempo1 + proj_instrument1 + proj_note_name1 + proj_octave1 + proj_duration1
-        f = f/6
         ff = torch.nn.functional.one_hot(x[:, 1:, 1], num_classes=6914) + torch.nn.functional.one_hot(x[:, 1:, 2], num_classes=6914) + torch.nn.functional.one_hot(x[:, 1:, 3], num_classes=6914) + torch.nn.functional.one_hot(x[:, 1:, 4], num_classes=6914) + torch.nn.functional.one_hot(x[:, 1:, 5], num_classes=6914) + torch.nn.functional.one_hot(x[:, 1:, 6], num_classes=6914)
-        ff = ff/6
-        loss1 = calculate_loss1(f, ff.float(), type_mask(target)) *0.5
+        loss1 = calculate_loss1(f, ff.float(), type_mask(target)) *0.1
         
         return type_loss, barbeat_loss, tempo_loss, instrument_loss, note_name_loss, octave_loss, duration_loss, loss1
    
