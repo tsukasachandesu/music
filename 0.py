69fc7af86863cb33243927d3a9616f492d9350a0
tsukasachandesu <44788261+tsukasachandesu@users.noreply.github.com>
2023-06-26 03:02:30 +0900

    Update 0.py

--- 0.py
+++ 0.py
@@ -69,9 +69,9 @@ def add_argument():
                         help='use CPU in case there\'s no GPU support')
     parser.add_argument('--use_ema', default=False, action='store_true',
                         help='whether use exponential moving average')
-    parser.add_argument('-b', '--batch_size', default=4, type=int,
+    parser.add_argument('-b', '--batch_size', default=5, type=int,
                         help='mini-batch size (default: 32)')
-    parser.add_argument('-e', '--epochs', default=2, type=int,
+    parser.add_argument('-e', '--epochs', default=1, type=int,
                         help='number of total epochs (default: 30)')
     parser.add_argument('--local_rank', type=int, default=-1,
                        help='local rank passed from distributed launcher')
@@ -83,7 +83,7 @@ def add_argument():
 # constants
 
 EPOCHS = 1
-GRADIENT_ACCUMULATE_EVERY =4
+GRADIENT_ACCUMULATE_EVERY = 5
 GENERATE_EVERY = 1800
 GENERATE_LENGTH = 1024
 yes = None
