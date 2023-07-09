ffbdcc27136654401ee5ecf3ad5ecdb638b4333d
tsukasachandesu <44788261+tsukasachandesu@users.noreply.github.com>
2023-07-09 20:09:05 +0900

    Update compound_word_transformer_wrapper.py

--- mgt/models/compound_word_transformer/compound_word_transformer_wrapper.py
+++ mgt/models/compound_word_transformer/compound_word_transformer_wrapper.py
@@ -161,7 +161,7 @@ class CompoundWordTransformerWrapper(nn.Module):
         pe = torch.zeros(max_seq_len, 1, 512)
         pe[:, 0, 0::2] = torch.sin(position * div_term)
         pe[:, 0, 1::2] = torch.cos(position * div_term)
-
+        self.register_buffer('pe', pe)
         self.init_()
 
     def init_(self):
