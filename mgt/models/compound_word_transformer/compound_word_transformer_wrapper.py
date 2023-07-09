5b1bce164f7f00f57045f09d985e5f26a93bd469
tsukasachandesu <44788261+tsukasachandesu@users.noreply.github.com>
2023-07-09 18:25:06 +0900

    Update compound_word_transformer_wrapper.py

--- mgt/models/compound_word_transformer/compound_word_transformer_wrapper.py
+++ mgt/models/compound_word_transformer/compound_word_transformer_wrapper.py
@@ -110,7 +110,7 @@ class CompoundWordTransformerWrapper(nn.Module):
 
         dim = attn_layers.dim
         emb_dim = default(emb_dim, dim)
-
+        self.word_emb_type = CompoundTransformerEmbeddings(self.num_tokens[0], self.emb_sizes[0])
         self.num_tokens = num_tokens
         self.max_seq_len = max_seq_len
         self.type1 = CompoundTransformerEmbeddings(14, 256)
