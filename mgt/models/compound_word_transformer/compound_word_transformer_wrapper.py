f58bae31fdad961d9cfbe2af56b55f0faf5136e6
tsukasachandesu <44788261+tsukasachandesu@users.noreply.github.com>
2023-07-12 20:38:08 +0900

    Update compound_word_transformer_wrapper.py

--- mgt/models/compound_word_transformer/compound_word_transformer_wrapper.py
+++ mgt/models/compound_word_transformer/compound_word_transformer_wrapper.py
@@ -263,10 +263,7 @@ class CompoundWordTransformerWrapper(nn.Module):
         z = self.enc_attn1(z, mask=None, return_hiddens=False)
 	    
         latents = self.lat_emb(torch.arange(self.max_seq_len-1, device = x.device))	    
-        latents = latents.repeat(x.shape[0], 1, 1)
-
-        print(latents.shape)
-        letents = latents.reshape(-1,1,512)
+        latents = latents.repeat(x.shape[0], 1, 1).reshape(-1,1,512)
 
         print(latents.shape)
         print("aaaaa")
