64f47185f85cd2d8571a6397fe2644a662d6f3ef
tsukasachandesu <44788261+tsukasachandesu@users.noreply.github.com>
2023-07-15 10:17:07 +0900

    Update compound_word_transformer_wrapper.py

--- mgt/models/compound_word_transformer/compound_word_transformer_wrapper.py
+++ mgt/models/compound_word_transformer/compound_word_transformer_wrapper.py
@@ -323,7 +323,7 @@ class CompoundWordTransformerWrapper(nn.Module):
         print(z.shape)
         z = z + self.pos_emb(z)
         print(z.shape)
-        latents = self.lat_emb(torch.arange(x2/16,device = x.device))	
+        latents = self.lat_emb(torch.arange(32, device = x.device))	
         print(latents.shape)
         latents = latents.repeat(x1, 1, 1).reshape(-1,1,512)
         latents = latents + self.pos_emb(latents)
