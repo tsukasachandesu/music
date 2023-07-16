3e8969df80ef4004a2ad3fe550cf4cfdbdc57022
tsukasachandesu <44788261+tsukasachandesu@users.noreply.github.com>
2023-07-16 12:23:45 +0900

    Update compound_word_transformer_model.py

--- mgt/models/compound_word_transformer_model.py
+++ mgt/models/compound_word_transformer_model.py
@@ -142,7 +142,7 @@ class CompoundWordTransformerModel(object):
             max_seq_len=self.max_sequence_length,
             attn_layers=Decoder(
                 dim=self.dim,
-                depth=12,
+                depth=6,
                 heads=self.heads,
                 ff_glu = True,
                 ff_swish = True,
@@ -157,7 +157,7 @@ class CompoundWordTransformerModel(object):
             ),
             attn_layers1=CrossAttender(
                 dim=512,
-                depth=12,
+                depth=6,
                 heads=10,
                 ff_glu = True,
                 ff_swish = True,
