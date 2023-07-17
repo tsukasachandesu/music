036c590a8a532b1d2e6415beb3f6e9ae1e7bfd20
tsukasachandesu <44788261+tsukasachandesu@users.noreply.github.com>
2023-07-17 19:52:14 +0900

    Update compound_word_autoregressive_wrapper.py

--- mgt/models/compound_word_transformer/compound_word_autoregressive_wrapper.py
+++ mgt/models/compound_word_transformer/compound_word_autoregressive_wrapper.py
@@ -147,13 +147,11 @@ class CompoundWordAutoregressiveWrapper(nn.Module):
         proj_type1 = self.soft(proj_barbeat)
         x1,x2,x3 = proj_type1.shape
         b = proj_type1[:,:,1:].unsqueeze(3).reshape(x1,x2,64,-1)
-        print(b.shape)
         b = torch.sum(b,-1)
-        print(b.shape)
         
         x1,x2,x3 = proj_type1.shape
         
-        a = torch.cat([proj_type1[:,:,0],b],-1)
+        a = torch.cat([proj_type1[:,:,0].squeeze(2),b],-1)
         print(a.shape)
         a1 = calculate_loss(a1, i_tensor[..., 0], type_mask(target))
         a3 = calculate_loss(a3, k_tensor[..., 0], type_mask(target))
