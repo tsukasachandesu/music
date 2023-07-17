c1d78970440bf185b7d7207c3fad51ea7f214b90
tsukasachandesu <44788261+tsukasachandesu@users.noreply.github.com>
2023-07-17 19:40:10 +0900

    Update compound_word_autoregressive_wrapper.py

--- mgt/models/compound_word_transformer/compound_word_autoregressive_wrapper.py
+++ mgt/models/compound_word_transformer/compound_word_autoregressive_wrapper.py
@@ -143,10 +143,14 @@ class CompoundWordAutoregressiveWrapper(nn.Module):
         note_name_loss = calculate_loss(proj_note_name, target[..., 4], type_mask(target))
         octave_loss = calculate_loss(proj_octave, target[..., 5], type_mask(target))
         duration_loss = calculate_loss(proj_duration, target[..., 6], type_mask(target))
-        
         proj_type1 = self.soft(proj_type)
-        x1,x2,x3 = proj_type.shape
-        a = torch.cat([proj_type1[:,:,1:],torch.sum(proj_type1[:,:,1:].unsqueeze(3).reshape(x1,x2,64,-1),-1).squeeze(3)],-1)
+        b = proj_type1[:,:,1:]
+        print(b.shape)
+        b = b.unsqueeze(3)
+        print(b.shape)
+        x1,x2,x3 = proj_type1.shape
+        
+        a = torch.cat([proj_type1[:,:,1:],torch.sum(b.reshape(x1,x2,64,-1),-1).squeeze(3)],-1)
         print(a.shape)
         a1 = calculate_loss(a1, i_tensor[..., 0], type_mask(target))
         a3 = calculate_loss(a3, k_tensor[..., 0], type_mask(target))
