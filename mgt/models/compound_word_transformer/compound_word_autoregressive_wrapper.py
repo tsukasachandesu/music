f67797bf7aff05e801b21bc604f2d287e402360d
tsukasachandesu <44788261+tsukasachandesu@users.noreply.github.com>
2023-07-17 20:57:42 +0900

    Update compound_word_autoregressive_wrapper.py

--- mgt/models/compound_word_transformer/compound_word_autoregressive_wrapper.py
+++ mgt/models/compound_word_transformer/compound_word_autoregressive_wrapper.py
@@ -150,7 +150,7 @@ class CompoundWordAutoregressiveWrapper(nn.Module):
         
         proj = torch.cat([proj_barbeat.unsqueeze(3), proj_tempo.unsqueeze(3), proj_instrument.unsqueeze(3), proj_note_name.unsqueeze(3), proj_octave.unsqueeze(3), proj_duration.unsqueeze(3)],-1)
         print(proj.shape)
-        proj = proj[:,1:,0].reshape(-1,x2,x3,1)
+        proj = proj[:,:,1:,:].reshape(-1,x2,x3,1)
         print(proj.shape)
         x1,x2,x3 ,x4= proj.shape
         proj = proj.reshape(x1,x2,64,-1)
@@ -160,7 +160,7 @@ class CompoundWordAutoregressiveWrapper(nn.Module):
         proj = torch.cat([proj[:,:,0].unsqueeze(2), proj1],-1)
         print(proj.shape)
         print( type_mask(target.repeat((6,1))))
-        proj = calculate_loss(proj, k_tensor.reshape(-1,x2,1).squeeze(2), type_mask(target.repeat((6,1))))
+        proj = calculate_loss(proj, k_tensor.reshape(-1,x2,1).squeeze(2), type_mask(target.repeat((6,1,1))))
         
         return type_loss, barbeat_loss, tempo_loss, instrument_loss, note_name_loss, octave_loss, duration_loss
    
