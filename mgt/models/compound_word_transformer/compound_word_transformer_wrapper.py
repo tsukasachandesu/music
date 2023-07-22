dcf6880c4d03dc5b34a3e9d0271d96e6e032d77c
tsukasachandesu <44788261+tsukasachandesu@users.noreply.github.com>
2023-07-23 07:47:11 +0900

    Update compound_word_transformer_wrapper.py

--- mgt/models/compound_word_transformer/compound_word_transformer_wrapper.py
+++ mgt/models/compound_word_transformer/compound_word_transformer_wrapper.py
@@ -346,6 +346,7 @@ class CompoundWordTransformerWrapper(nn.Module):
         x = self.attn_layers2(x, context = y, mask = mask.reshape(-1,1), context_mask = mask.reshape(-1,1).repeat((1,7)))
         x = x.reshape(x1,-1,512)
         x = x + self.pos_emb1(x) + self.emb(x[..., 0])
+        x = self.emb_dropout(x)
         x = self.attn_layers1(x, mask = None)
 	    
         return x, self.proj_type(x)
