65e47391caba5dfc1b95121ba4821401fbf62118
tsukasachandesu <44788261+tsukasachandesu@users.noreply.github.com>
2023-07-28 08:47:41 +0900

    Update compound_word_autoregressive_wrapper.py

--- mgt/models/compound_word_transformer/compound_word_autoregressive_wrapper.py
+++ mgt/models/compound_word_transformer/compound_word_autoregressive_wrapper.py
@@ -9,10 +9,67 @@ from mgt.models.utils import get_device
 from einops import rearrange, reduce, repeat
 
 import torch
+from torch.nn import Module, Softmax
 from typing import Optional
+
+def log(t, eps = 1e-20):
+    return t.clamp(min = eps).log()
+
+def gumbel_noise(t):
+    noise = torch.zeros_like(t).uniform_(0, 1)
+    return -log(-log(noise))
+
+def gumbel_sample(t, temperature = 1., dim = -1):
+    return ((t / max(temperature, 1e-10)) + gumbel_noise(t)).argmax(dim = dim)
+
+def top_k(logits, thres = 0.9):
+    k = int((1 - thres) * logits.shape[-1])
+    val, ind = torch.topk(logits, k)
+    probs = torch.full_like(logits, -torch.finfo(logits.dtype).max)
+    probs.scatter_(1, ind, val)
+    return probs
 
 def type_mask(target):
     return target[..., 0] != 0
+
+def calculate_loss1(predicted, target, loss_mask):
+    trainable_values = torch.sum(loss_mask)
+    if trainable_values == 0:
+        return 0
+
+    loss = F.mse_loss(predicted[:, ...], target, reduction = 'none')
+    loss = loss * loss_mask.unsqueeze(-1)
+    loss = torch.sum(loss) / trainable_values
+
+    return loss
+
+def notes_to_ce(indices):
+  note_index_to_pitch_index = [0, -5, 2, -3, 4, -1, -6, 1, -4, 3, -2, 5]
+  total = np.zeros(3)
+  count = 0
+  for index in indices:
+    total += pitch_index_to_position(note_index_to_pitch_index[index])
+    count += 1
+  if count != 0:
+    total /= count               
+  return total.tolist()    
+
+def pitch_index_to_position(pitch_index) :
+    c = pitch_index - (4 * (pitch_index // 4))
+    verticalStep = 0.4
+    radius = 1.0
+    pos = np.array([0.0, 0.0, 0.0])
+    if c == 0:
+        pos[1] = radius
+    if c == 1:
+        pos[0] = radius
+    if c == 2:
+        pos[1] = -1*radius
+    if c == 3:
+        pos[0] = -1*radius
+    pos[2] = pitch_index * verticalStep
+    
+    return np.array(pos)
 
 def calculate_loss(predicted, target, loss_mask):
     trainable_values = torch.sum(loss_mask)
@@ -33,6 +90,7 @@ class CompoundWordAutoregressiveWrapper(nn.Module):
         self.ignore_index = ignore_index
         self.net = net
         self.max_seq_len = net.max_seq_len
+        self.soft = nn.Softmax()
 
     @torch.no_grad()
     def generate(self, prompt, output_length=100, selection_temperatures=None, selection_probability_tresholds=None):
@@ -75,7 +133,6 @@ class CompoundWordAutoregressiveWrapper(nn.Module):
         duration_loss = calculate_loss(proj_duration, target[..., 6], type_mask(target))
         
         return type_loss, barbeat_loss, tempo_loss, instrument_loss, note_name_loss, octave_loss, duration_loss
-   
    
    
    
