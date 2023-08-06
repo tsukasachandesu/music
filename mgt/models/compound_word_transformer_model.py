fe6d2669da1c18c4fd16733aa10aaf82c129c0b2
tsukasachandesu <44788261+tsukasachandesu@users.noreply.github.com>
2023-08-06 20:52:30 +0900

    Update compound_word_transformer_model.py

--- mgt/models/compound_word_transformer_model.py
+++ mgt/models/compound_word_transformer_model.py
@@ -37,9 +37,9 @@ defaults = {
     'max_sequence_length': 1024,
     'learning_rate': 1e-4,
     'dropout': 0.1,
-    'dim': 512,
+    'dim': 768,
     'depth': 32,
-    'heads': 8
+    'heads': 12
 }
 
 
@@ -73,11 +73,11 @@ class CompoundWordTransformerModel(object):
     def train(self,
               x_train,
               epochs,
-              batch_size=4,
+              batch_size=6,
               stop_loss=None,
               batches_per_epoch=100,
               report_per_x_batches=20,
-              gradient_accumulation_steps=1):
+              gradient_accumulation_steps=2):
         self.model.train()
         start_time = time.time()
         for epoch in range(epochs):
