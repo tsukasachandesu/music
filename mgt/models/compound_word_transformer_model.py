dbee523c9548e3fe32db688ca739a7f57b0dca5e
tsukasachandesu <44788261+tsukasachandesu@users.noreply.github.com>
2023-08-11 17:49:51 +0900

    Update compound_word_transformer_model.py

--- mgt/models/compound_word_transformer_model.py
+++ mgt/models/compound_word_transformer_model.py
@@ -150,7 +150,8 @@ class CompoundWordTransformerModel(object):
                 ff_dropout=self.dropout,  # feedforward dropout
                 ff_no_bias = True,
                 attn_one_kv_head = True,
-                rotary_pos_emb = True
+                rotary_pos_emb = True,
+                rel_pos_bias = True
             ),
             attn_layers1=Encoder(
                 dim=self.dim,
@@ -164,7 +165,7 @@ class CompoundWordTransformerModel(object):
                 ff_dropout=self.dropout,  # feedforward dropout
                 ff_no_bias = True,
                 attn_one_kv_head = True,
-                rotary_pos_emb = True
+                rel_pos_bias = True
             )
         )).to(get_device())
 
