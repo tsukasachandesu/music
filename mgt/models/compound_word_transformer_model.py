e5dc75fe50a2316759a725a5154843e445a976ef
tsukasachandesu <44788261+tsukasachandesu@users.noreply.github.com>
2023-08-21 22:08:22 +0900

    Update compound_word_transformer_model.py

--- mgt/models/compound_word_transformer_model.py
+++ mgt/models/compound_word_transformer_model.py
@@ -73,7 +73,7 @@ class CompoundWordTransformerModel(object):
     def train(self,
               x_train,
               epochs,
-              batch_size=6,
+              batch_size=12,
               stop_loss=None,
               batches_per_epoch=100,
               report_per_x_batches=20,
