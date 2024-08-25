8c228d0e9feaba3351ec3beed08110ef320e8077
tsukasachandesu <44788261+tsukasachandesu@users.noreply.github.com>
2024-08-25 11:46:49 +0900

    Update compound_word_autoregressive_wrapper.py

--- mgt/models/compound_word_transformer/compound_word_autoregressive_wrapper.py
+++ mgt/models/compound_word_transformer/compound_word_autoregressive_wrapper.py
@@ -67,7 +67,7 @@ class CompoundWordAutoregressiveWrapper(nn.Module):
         target = x[:, 1:, :]
 
         h, proj_type = self.net.forward_hidden(xi, **kwargs)
-        proj_barbeat, proj_tempo, proj_instrument, proj_note_name, proj_octave, proj_duration, proj_velocity = self.net.forward_output(
+        proj_barbeat, proj_tempo, proj_instrument, proj_note_name, proj_octave = self.net.forward_output(
             h, target)
         # Filter padding indices
         type_loss = calculate_loss(proj_type, target[..., 0], type_mask(target))
@@ -76,7 +76,5 @@ class CompoundWordAutoregressiveWrapper(nn.Module):
         instrument_loss = calculate_loss(proj_instrument, target[..., 3], type_mask(target))
         note_name_loss = calculate_loss(proj_note_name, target[..., 4], type_mask(target))
         octave_loss = calculate_loss(proj_octave, target[..., 5], type_mask(target))
-        duration_loss = calculate_loss(proj_duration, target[..., 6], type_mask(target))
-        velocity_loss = calculate_loss(proj_velocity, target[..., 7], type_mask(target))
 
-        return type_loss, barbeat_loss, tempo_loss, instrument_loss, note_name_loss, octave_loss, duration_loss, velocity_loss
+        return type_loss, barbeat_loss, tempo_loss, instrument_loss, note_name_loss, octave_loss
