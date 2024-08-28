a2659babd58c9cfc621899ea0849a9a591d407b9
tsukasachandesu <44788261+tsukasachandesu@users.noreply.github.com>
2024-08-28 22:25:14 +0900

    Update compound_word_transformer_wrapper.py

--- mgt/models/compound_word_transformer/compound_word_transformer_wrapper.py
+++ mgt/models/compound_word_transformer/compound_word_transformer_wrapper.py
@@ -1,1 +1,230 @@
+##################################
+# -- temperature -- #
+import numpy as np
+import torch
+from torch import nn
+from x_transformers.x_transformers import AttentionLayers, default, AbsolutePositionalEmbedding, always
 
+from mgt.models.compound_word_transformer.compound_transformer_embeddings import CompoundTransformerEmbeddings
+from mgt.models.utils import get_device
+
+
+def softmax_with_temperature(logits, temperature):
+    probs = np.exp(logits / temperature) / np.sum(np.exp(logits / temperature))
+    return probs
+
+
+def weighted_sampling(probs):
+    probs /= sum(probs)
+    sorted_probs = np.sort(probs)[::-1]
+    sorted_index = np.argsort(probs)[::-1]
+    word = np.random.choice(sorted_index, size=1, p=sorted_probs)[0]
+    return word
+
+
+# -- nucleus -- #
+def nucleus(probs, probability_treshold):
+    probs /= (sum(probs) + 1e-5)
+    sorted_probs = np.sort(probs)[::-1]
+    sorted_index = np.argsort(probs)[::-1]
+    cusum_sorted_probs = np.cumsum(sorted_probs)
+    after_threshold = cusum_sorted_probs > probability_treshold
+    if sum(after_threshold) > 0:
+        last_index = np.where(after_threshold)[0][0] + 1
+        candi_index = sorted_index[:last_index]
+    else:
+        candi_index = sorted_index[:]
+    candi_probs = [probs[i] for i in candi_index]
+    candi_probs /= sum(candi_probs)
+    word = np.random.choice(candi_index, size=1, p=candi_probs)[0]
+    return word
+
+
+def sampling(logit, probability_treshold=None, temperature=1.0):
+    logit = logit.squeeze().cpu().detach().numpy()
+    probs = softmax_with_temperature(logits=logit, temperature=temperature)
+
+    if probability_treshold is not None:
+        cur_word = nucleus(probs, probability_treshold=probability_treshold)
+    else:
+        cur_word = weighted_sampling(probs)
+    return cur_word
+
+
+class CompoundWordTransformerWrapper(nn.Module):
+    def __init__(
+            self,
+            *,
+            num_tokens,
+            max_seq_len,
+            attn_layers,
+            emb_dim=None,
+            emb_dropout=0.,
+            use_pos_emb=True,
+            emb_sizes=None
+    ):
+        super().__init__()
+
+        if emb_sizes is None:
+            emb_sizes = [
+        96,   # Bar / Beat
+        512,  # Instrument
+        128,  # Note Name
+        128,  # Octave
+        256,  # Duration
+            ]
+
+        self.emb_sizes = emb_sizes
+
+        dim = attn_layers.dim
+        emb_dim = default(emb_dim, dim)
+
+        self.num_tokens = num_tokens
+        self.max_seq_len = max_seq_len
+
+        self.word_emb_type = CompoundTransformerEmbeddings(self.num_tokens[0], self.emb_sizes[0])
+        self.word_emb_barbeat = CompoundTransformerEmbeddings(self.num_tokens[1], self.emb_sizes[1])
+        self.word_emb_tempo = CompoundTransformerEmbeddings(self.num_tokens[2], self.emb_sizes[2])
+        self.word_emb_instrument = CompoundTransformerEmbeddings(self.num_tokens[3], self.emb_sizes[3])
+        self.word_emb_note_name = CompoundTransformerEmbeddings(self.num_tokens[4], self.emb_sizes[4])
+
+        # individual output
+        self.proj_type = nn.Linear(dim, self.num_tokens[0])
+        self.proj_barbeat = nn.Linear(dim, self.num_tokens[1])
+        self.proj_tempo = nn.Linear(dim, self.num_tokens[2])
+        self.proj_instrument = nn.Linear(dim, self.num_tokens[3])
+        self.proj_note_name = nn.Linear(dim, self.num_tokens[4])
+
+        # in_features is equal to dimension plus dimensions of the type embedding
+        self.project_concat_type = nn.Linear(dim + self.emb_sizes[0], dim)
+
+        self.compound_word_embedding_size = np.sum(emb_sizes)
+
+        self.pos_emb = AbsolutePositionalEmbedding(emb_dim, self.max_seq_len) 
+        self.emb_dropout = nn.Dropout(emb_dropout)
+
+        self.project_emb = nn.Linear(emb_dim, dim) if emb_dim != dim else nn.Identity()
+        self.attn_layers = attn_layers
+        self.norm = nn.LayerNorm(dim)
+
+        self.in_linear = nn.Linear(np.sum(self.emb_sizes), emb_dim)
+
+        self.init_()
+
+    def init_(self):
+        nn.init.normal_(self.word_emb_type.weight(), std=0.02)
+        nn.init.normal_(self.word_emb_barbeat.weight(), std=0.02)
+        nn.init.normal_(self.word_emb_tempo.weight(), std=0.02)
+        nn.init.normal_(self.word_emb_instrument.weight(), std=0.02)
+        nn.init.normal_(self.word_emb_note_name.weight(), std=0.02)
+
+    def forward_output_sampling(self, h, y_type, selection_temperatures=None, selection_probability_tresholds=None):
+        # sample type
+        if selection_probability_tresholds is None:
+            selection_probability_tresholds = {}
+
+        if selection_temperatures is None:
+            selection_temperatures = {}
+
+        y_type_logit = y_type[0, :]
+        cur_word_type = sampling(
+            y_type_logit,
+            probability_treshold=selection_probability_tresholds.get(0, None),
+            temperature=selection_temperatures.get(0, 1.0)
+        )
+
+        type_word_t = torch.from_numpy(np.array([cur_word_type])).long().to(get_device()).unsqueeze(0)
+
+        tf_skip_type = self.word_emb_type(type_word_t)
+
+        # concat
+        y_concat_type = torch.cat([h, tf_skip_type], dim=-1)
+        y_ = self.project_concat_type(y_concat_type)
+
+        # project other
+        proj_barbeat = self.proj_barbeat(y_)
+        proj_tempo = self.proj_tempo(y_)
+        proj_instrument = self.proj_instrument(y_)
+        proj_note_name = self.proj_note_name(y_)
+
+        # sampling gen_cond
+        cur_word_barbeat = sampling(
+            proj_barbeat,
+            probability_treshold=selection_probability_tresholds.get(1, None),
+            temperature=selection_temperatures.get(1, 1.0))
+
+        cur_word_tempo = sampling(
+            proj_tempo,
+            probability_treshold=selection_probability_tresholds.get(2, None),
+            temperature=selection_temperatures.get(2, 1.0))
+
+        cur_word_instrument = sampling(
+            proj_instrument,
+            probability_treshold=selection_probability_tresholds.get(3, None),
+            temperature=selection_temperatures.get(3, 1.0))
+
+        cur_word_note_name = sampling(
+            proj_note_name,
+            probability_treshold=selection_probability_tresholds.get(4, None),
+            temperature=selection_temperatures.get(4, 1.0))
+
+        # collect
+        next_arr = np.array([
+            cur_word_type,
+            cur_word_barbeat,
+            cur_word_tempo,
+            cur_word_instrument,
+            cur_word_note_name,
+        ])
+        return next_arr
+
+    def forward_output(self,
+                       h,
+                       target
+                       ):
+        tf_skip_type = self.word_emb_type(target[..., 0])
+
+        y_concat_type = torch.cat([h, tf_skip_type], dim=-1)
+        y_ = self.project_concat_type(y_concat_type)
+
+        proj_barbeat = self.proj_barbeat(y_)
+        proj_tempo = self.proj_tempo(y_)
+        proj_instrument = self.proj_instrument(y_)
+        proj_note_name = self.proj_note_name(y_)
+
+        return proj_barbeat, proj_tempo, proj_instrument, proj_note_name
+
+    def forward_hidden(
+            self,
+            x,
+            mask=None,
+            **kwargs
+    ):
+        # embeddings
+        emb_type = self.word_emb_type(x[..., 0])
+        emb_barbeat = self.word_emb_barbeat(x[..., 1])
+        emb_tempo = self.word_emb_tempo(x[..., 2])
+        emb_instrument = self.word_emb_instrument(x[..., 3])
+        emb_note_name = self.word_emb_note_name(x[..., 4])    
+
+        embs = torch.cat(
+            [
+                emb_type,
+                emb_barbeat,
+                emb_tempo,
+                emb_instrument,
+                emb_note_name,
+            ], dim=-1)
+
+        emb_linear = self.in_linear(embs)
+        x = emb_linear + self.pos_emb(emb_linear)
+        x = self.emb_dropout(x)
+        x = self.project_emb(x)
+
+        if not self.training:
+            x.squeeze(0)
+
+        x, intermediates = self.attn_layers(x, mask=mask, return_hiddens=True, **kwargs)
+        x = self.norm(x)
+
+        return x, self.proj_type(x)
